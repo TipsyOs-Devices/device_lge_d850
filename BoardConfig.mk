4c76a0b643e47dc6b68fcf1fd5f6853d14b35f0a
dblockopp <mattopp17@gmail.com>
2015-12-31 03:57:15 -0500

    try CLANG as true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 # Rom Toolchain
 TARGET_GCC_VERSION_EXP := 4.9-uber
 # Optimizations
-CLANG_O3 := false
+CLANG_O3 := true
 STRICT_ALIASING := false
 KRAIT_TUNINGS := false
 GRAPHITE_OPTS := false
